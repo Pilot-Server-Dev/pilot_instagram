02582304f27cf36b2acb84a6baa292f32a40bbc5
kitaee <dlrlxo999@naver.com>
2024-11-05 14:45:52 +0900

    feat: 유저 회원가입 테스트케이스 구현

---
+++ src/test/java/pilot/instagram/user/UserControllerTest.java
@@ -0,0 +1,46 @@
+package pilot.instagram.user;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import pilot.instagram.domain.user.UserController;
+import pilot.instagram.domain.user.request.UserRequest;
+import pilot.instagram.domain.user.service.UserService;
+
+@WebMvcTest(controllers = UserController.class)
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private UserService userService;
+
+    @Test
+    @DisplayName("신규 회원을 등록한다.")
+    void saveUser() throws Exception {
+        // given
+        UserRequest userRequest = UserRequest.builder().name("이기태").build();
+
+
+        // when, then
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/user/new")
+                        .content(objectMapper.writeValueAsString(userRequest))
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isCreated());
+    }
+}
