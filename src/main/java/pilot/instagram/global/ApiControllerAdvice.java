f3047957d4a9a275ab28662bacb1853bb15a465c
kitaee <dlrlxo999@naver.com>
2024-11-08 14:21:38 +0900

    fix: 디렉토리 위치 수정

--- src/main/java/pilot/instagram/global/ApiControllerAdvice.java
+++ src/main/java/pilot/instagram/global/ApiControllerAdvice.java
@@ -1,7 +1,6 @@
-package pilot.instagram.config;
+package pilot.instagram.global;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
