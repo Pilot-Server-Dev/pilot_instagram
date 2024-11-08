69cc3ce22d86081fb6ad81df69f75f2b3fc9b349
kitaee <dlrlxo999@naver.com>
2024-11-08 12:39:12 +0900

    feat: API Response 정의

---
+++ src/main/java/pilot/instagram/config/ApiResponse.java
@@ -0,0 +1,30 @@
+package pilot.instagram.config;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import pilot.instagram.domain.user.response.UserResponse;
+
+@Getter
+public class ApiResponse<T> {
+
+    private int code;
+    private HttpStatus status;
+    private String message;
+    private T data;
+
+    public ApiResponse(HttpStatus status, String message, T data) {
+        this.code = status.value();
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> ApiResponse<T> of(HttpStatus status, String message, T data) {
+        return new ApiResponse<>(status, message, data);
+    }
+
+    public static <T> ApiResponse<T> of(HttpStatus status, T data) {
+        return new ApiResponse<>(status, status.name(), data);
+    }
+}
