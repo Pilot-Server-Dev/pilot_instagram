05652ad14741606918f50a6ba0ee742908b1c714
kitaee <dlrlxo999@naver.com>
2024-11-08 12:44:44 +0900

    feat: 중복 회원가입에 대한 테스트 케이스 추가

--- src/main/java/pilot/instagram/domain/user/UserController.java
+++ src/main/java/pilot/instagram/domain/user/UserController.java
@@ -2,10 +2,10 @@ package pilot.instagram.domain.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import pilot.instagram.config.ApiResponse;
 import pilot.instagram.domain.user.request.UserRequest;
 import pilot.instagram.domain.user.response.UserResponse;
 import pilot.instagram.domain.user.service.UserService;
@@ -17,7 +17,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/api/user/new")
-    public ResponseEntity<UserResponse> saveUser(@RequestBody UserRequest userRequest) {
-        return new ResponseEntity<>(userService.saveUser(userRequest), HttpStatus.CREATED);
+    public ApiResponse<UserResponse> saveUser(@RequestBody UserRequest userRequest) {
+        return ApiResponse.of(HttpStatus.CREATED, userService.saveUser(userRequest));
     }
 }
