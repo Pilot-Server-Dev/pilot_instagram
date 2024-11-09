bada95b859501cb5e13b6cf3834ff6b39eefc27b
kitaee <dlrlxo999@naver.com>
2024-11-09 13:48:21 +0900

    fix: @RequestMapping 추가로 공통 API URI 묶는 것으로 수정

--- src/main/java/pilot/instagram/domain/user/UserController.java
+++ src/main/java/pilot/instagram/domain/user/UserController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pilot.instagram.global.ApiResponse;
 import pilot.instagram.domain.user.request.UserRequest;
@@ -12,11 +13,12 @@ import pilot.instagram.domain.user.service.UserService;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/user")
 public class UserController {
 
     private final UserService userService;
 
-    @PostMapping("/api/user/new")
+    @PostMapping("/new")
     public ApiResponse<UserResponse> saveUser(@RequestBody UserRequest userRequest) {
         return ApiResponse.of(HttpStatus.CREATED, userService.saveUser(userRequest));
     }
