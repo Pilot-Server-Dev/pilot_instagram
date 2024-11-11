ca87a5321328d2182d3a866d19f64f05b06e4ff6
kitaee <dlrlxo999@naver.com>
2024-11-11 12:49:12 +0900

    feat: 세션 로그인 구현(미완)

--- src/main/java/pilot/instagram/domain/user/UserController.java
+++ src/main/java/pilot/instagram/domain/user/UserController.java
@@ -1,12 +1,10 @@
 package pilot.instagram.domain.user;
 
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pilot.instagram.global.ApiResponse;
 import pilot.instagram.domain.user.request.UserRequest;
 import pilot.instagram.domain.user.response.UserResponse;
@@ -22,5 +20,16 @@ public class UserController {
     @PostMapping("/new")
     public ApiResponse<UserResponse> saveUser(@Valid @RequestBody UserRequest userRequest) {
         return ApiResponse.of(HttpStatus.CREATED, userService.saveUser(userRequest));
+    }
+
+    @PostMapping("/login")
+    public ApiResponse<String> login(@Valid @RequestBody UserRequest userRequest, HttpSession session) {
+        session.setAttribute("user", userRequest.getId());
+        return ApiResponse.of(HttpStatus.OK, session.getId());
+    }
+
+    @GetMapping("/test")
+    public String test(HttpSession httpSession) {
+        return httpSession.getAttribute("user").toString();
     }
 }
