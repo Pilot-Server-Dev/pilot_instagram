a237f8cfab0b0d8b7e2187bf9a8d857a5898b8dd
kitaee <dlrlxo999@naver.com>
2024-11-12 14:25:06 +0900

    feat: 게시글 생성 API 구현

--- src/main/java/pilot/instagram/domain/post/entity/Post.java
+++ src/main/java/pilot/instagram/domain/post/entity/Post.java
@@ -1,25 +1,31 @@
 package pilot.instagram.domain.post.entity;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import pilot.instagram.domain.association.PostTag;
 import pilot.instagram.domain.comment.Comment;
 import pilot.instagram.domain.common.BaseTimeEntity;
 import pilot.instagram.domain.like.Like;
 import pilot.instagram.domain.media.Media;
+import pilot.instagram.domain.post.dto.request.PostRequest;
 import pilot.instagram.domain.user.entity.User;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Entity
-@Getter
+@Entity @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post extends BaseTimeEntity {
 
     @Column(name = "post_id")
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String content;
 
     @ManyToOne
@@ -37,4 +43,17 @@ public class Post extends BaseTimeEntity {
 
     @OneToMany(mappedBy = "post")
     private List<PostTag> postTags = new ArrayList<>();
+
+    @Builder
+    private Post(String content, User user) {
+        this.content = content;
+        this.user = user;
+    }
+
+    public static Post fromDtoToUser(PostRequest postRequest, User user) {
+        return Post.builder()
+                .content(postRequest.getContent())
+                .user(user)
+                .build();
+    }
 }
