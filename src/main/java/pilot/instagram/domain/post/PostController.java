bb310d551881aeff657f3cc78c637ce16c59d944
kitaee <dlrlxo999@naver.com>
2024-11-12 14:25:06 +0900

    feat: 게시글 생성 API 구현

--- src/main/java/pilot/instagram/domain/post/PostController.java
+++ src/main/java/pilot/instagram/domain/post/PostController.java
@@ -1,12 +1,28 @@
 package pilot.instagram.domain.post;
 
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pilot.instagram.domain.post.dto.request.PostRequest;
+import pilot.instagram.domain.post.dto.response.PostResponse;
 import pilot.instagram.domain.post.service.PostService;
+import pilot.instagram.global.ApiResponse;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/post")
 public class PostController {
 
     private final PostService postService;
+
+    @PostMapping("/new")
+    public ApiResponse<PostResponse> createPost(@Valid @RequestBody PostRequest postRequest, HttpSession session) {
+        String userId = session.getAttribute("userId").toString();
+        return ApiResponse.of(HttpStatus.CREATED, postService.savePost(postRequest, userId));
+    }
 }
--- src/main/java/pilot/instagram/domain/post/service/PostService.java
+++ src/main/java/pilot/instagram/domain/post/service/PostService.java
@@ -2,11 +2,26 @@ package pilot.instagram.domain.post.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import pilot.instagram.domain.post.dto.request.PostRequest;
+import pilot.instagram.domain.post.dto.response.PostResponse;
+import pilot.instagram.domain.post.entity.Post;
 import pilot.instagram.domain.post.repository.PostRepository;
+import pilot.instagram.domain.user.entity.User;
+import pilot.instagram.domain.user.repository.UserRepository;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class PostService {
 
+    private final UserRepository userRepository;
     private final PostRepository postRepository;
+
+    @Transactional
+    public PostResponse savePost(PostRequest postRequest, String userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("아이디를 찾을 수 없습니다"));
+        return PostResponse.of(postRepository.save(Post.fromDtoToUser(postRequest, user)));
+    }
 }
