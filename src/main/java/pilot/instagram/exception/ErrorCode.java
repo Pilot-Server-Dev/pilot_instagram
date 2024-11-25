e208e07e13c52e8b82f9a6a21d29d0cd60dc57d3
kitaee <dlrlxo999@naver.com>
2024-11-26 05:37:26 +0900

    add: 게시글 삭제 권한 관련 예외코드 추가

--- src/main/java/pilot/instagram/exception/ErrorCode.java
+++ src/main/java/pilot/instagram/exception/ErrorCode.java
@@ -12,7 +12,8 @@ public enum ErrorCode {
     USER_NOT_FOUND("아이디를 찾을 수 없습니다"),
 
     // POST
-    POST_NOT_FOUND("게시글을 찾을 수 없습니다.");
+    POST_NOT_FOUND("게시글을 찾을 수 없습니다."),
+    UNAUTHORIZED_POST_DELETE("게시글 삭제 권한이 없습니다.");
 
     private final String message;
 }
